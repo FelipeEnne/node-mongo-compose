1120d2bddd1efd69252004414a3e103b8d5341bc
FelipeEnne <felipeenne@gmail.com>
2022-04-16 13:26:31 -0300

    add crud to the project

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const restful = require("node-restful");
-const server = express();
-const mongoose = restful.mongoose;
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+const server = express();
+
+// db
+const mongoose = restful.mongoose;
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://db/mydb");
 
-server.get("/", (req, res, next) => res.send("Backend"));
+// middleware
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(cors());
+
+//DOM
+const Client = restful.model("Client", {
+  name: { type: String, required: true },
+});
+
+// rest api
+Client.methods(["get", "post", "put", "delete"]);
+Client.updateOptions({ new: true, runValidators: true });
+
+// routes
+Client.register(server, "/clients");
 
 server.listen(3000);
